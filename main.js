b7da363d6e300604fc7ad46c27196c264e94c674
Zeresenayaregal <zeresenayyaregal4@gmail.com>
2024-06-08 03:15:23 -0700

    JS completed

--- main.js
+++ main.js
@@ -48,3 +48,73 @@ const winFunction = (letter) => {
 		msgOut.innerHTML = "&#x1F389; <br> 'O' Wins";
 	}
 };
+
+//Function for draw
+const drawFunction = () => {
+	disableBtn();
+	msgOut.innerHTML = "&#x1F60E; <br> It's a Draw";
+};
+
+//New Game
+newGameBtn.addEventListener("click", () => {
+	count = 0;
+	enableBtn();
+});
+restartBtn.addEventListener("click", () => {
+	count = 0;
+	enableBtn();
+});
+
+//Win Logic
+const winChecker = () => {
+	//Loop through all win patterns
+	for (let i of winningPattern) {
+		let [element1, element2, element3] = [
+			gridBtn[i[0]].innerText,
+			gridBtn[i[1]].innerText,
+			gridBtn[i[2]].innerText,
+		];
+		//Check if elements are filled
+		//If 3 empty elements are same and would give win as would
+		if (element1 != "" && (element2 != "") & (element3 != "")) {
+			if (element1 == element2 && element2 == element3) {
+				//If all 3 buttons have same values then pass the value to winFunction
+				winFunction(element1);
+			}
+		}
+	}
+};
+
+//Display X/O on click
+gridBtn.forEach((element) => {
+	element.addEventListener("click", () => {
+		if (xTurn) {
+			xTurn = false;
+			//Display X
+			element.innerText = "X";
+			element.disabled = true;
+		} else {
+			xTurn = true;
+			//Display Y
+			element.innerText = "O";
+			element.disabled = true;
+		}
+		//Increment count on each click
+		count += 1;
+		if (count == 9) {
+			drawFunction();
+		}
+		//Check for win on every click
+		winChecker();
+	});
+});
+
+startBtn.addEventListener("click", () => {
+	enableBtn();
+	landing.innerHTML = `
+        <style> 
+            .landing {
+                display: none;
+            }
+        </style>`;
+});
